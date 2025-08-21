9ec298d31a8e16e9f675fe326be7059632cd1c3d
Empika1 <dravidts1@gmail.com>
2025-08-21 18:11:46 -0400

    god dammit why is this so laggy

--- makeStars.py
+++ makeStars.py
@@ -18,8 +18,15 @@ minPaddingDiff = 0
 maxX = 1920
 maxY = 1920
 
-layer = 0
-while layer < numLayers:
+starsymbol = """    <symbol id="star" viewBox="0 0 24 24">
+        <path d="M12 2l3 7h7l-6 4 2 7-6-4-6 4 2-7-6-4h7z"/>
+    </symbol>"""
+
+styletemp = """    <style>
+        use { fill: gold; }
+    </style>"""
+
+for layer in range(numLayers):
     bgLayer = bgLayerStart + layer * bgLayerDiff
     bgStarLayer = bgStarLayerStart + layer * bgStarLayerDiff
 
@@ -28,32 +35,31 @@ while layer < numLayers:
     maxSize = maxSizePerLayer + layer * maxSizeDiff
     minPadding = minPadding + layer * minPaddingDiff
 
-    print(f'<div class="bg{bgLayer}">')
-    print(f'<svg class="bg{bgStarLayer}-stars" xmlns="http://www.w3.org/2000/svg">')
+    with open(f"starsbg{bgLayer}.svg", "w") as f:
+        stars = []
 
-    stars = []
+        i = 0
+        while i < numStars:
+            x = random.randint(0, maxX)
+            y = random.randint(0, maxY)
+            size = random.randint(minSize, maxSize)
+            rotation = random.randint(0, 360)
 
-    i = 0
-    while i < numStars:
-        x = random.randint(0, maxX)
-        y = random.randint(0, maxY)
-        size = random.randint(minSize, maxSize)
-        rotation = random.randint(0, 360)
+            fail = False
+            for star in stars:
+                if (x - star[0]) ** 2 + (y - star[1]) ** 2 < (size + star[2] + minPadding) ** 2:
+                    fail = True
+                    break
+            if not fail:
+                stars.append((x, y, size, rotation))
+                i += 1
 
-        fail = False
+        f.write(f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {maxX} {maxY}">\n')
+        f.write(f"{starsymbol}\n")
+        f.write(f"{styletemp}\n")
+
         for star in stars:
-            if (x - star[0]) ** 2 + (y - star[1]) ** 2 < (size + star[2] + minPadding) ** 2:
-                fail = True
-                break
-        if not fail:
-            stars.append((x, y, size, rotation))
-            i += 1
-
-    for star in stars:
-        print(f'<use href="#star" x={star[0]}px y={star[1]}px width={star[2]}px height={star[2]}px ' +
-            f'style="transform-origin: {star[0] + star[2] // 2}px {star[1] + star[2] // 2}px; transform: rotate({star[3]}deg);"/>')
-
-    print("</svg>")
-    print("</div>")
-
-    layer += 1
+            f.write(f'    <use href="#star" x="{star[0]}" y="{star[1]}" width="{star[2]}" height="{star[2]}" ' +
+                  f'transform="rotate({star[3]}deg {star[0] + star[2] // 2} {star[1] + star[2] // 2})" />\n')
+        
+        f.write('</svg>')
