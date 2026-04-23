1a17216c1acb59fff833c7036a8d834ff07cdb05
empika <dravidts1@gmail.com>
2026-04-23 16:54:28 -0400

    clean lots of things up

---
+++ custom_elements.js
@@ -0,0 +1,37 @@
+class DiamondDiv extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.innerHTML =
+      `<link rel="stylesheet" href="styles.css">
+      <div class="box">
+        <div class="diamond-decal-tl"></div>
+        <div class="diamond-decal-tr"></div>
+        <div class="diamond-decal-bl"></div>
+        <div class="diamond-decal-br"></div>
+        <slot></slot>
+      </div>`;
+  }
+}
+
+class ProjectIcon extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const iconTypes = this.getAttribute("types").split(" ");
+    this.innerHTML = iconTypes.map(type =>
+      `<span class="project-icon ${type}-project">
+        <svg>
+          <use href="#icon-${type}"></use>
+        </svg>
+      </span>`).join(" ");
+  }
+}
+
+customElements.define("diamond-div", DiamondDiv);
+customElements.define("project-icon", ProjectIcon);
